998bc51d14feac6ff79bad917e3ae26175221113
Ryan Kresse <rkresse53@gmail.com>
2015-06-22 11:57:18 -0400

    refactor test for connected Components

--- test/connectedComponents.spec.js
+++ test/connectedComponents.spec.js
@@ -36,9 +36,6 @@ describe('Connected Components:UT', function(){
           expect(connComps[i][j]).to.equal(expected[i][j]);
         }
       }
-
-      // expect(connectedComps(graph)).to.equal("Invalid input. Graph must be a non-empty array.");
-    //[ [ 0, 2, 4, 6, 8 ], [ 1, 3 ], [ 5, 7, 9 ] ]
     });
 
   });
